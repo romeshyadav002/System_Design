fcb6e9975f74db505da56d9dcd3a941d21ee2302
romeshyadav002 <raojiromesh002@gmail.com>
2025-04-27 11:34:45 +0530

    ATM System Design

---
+++ ATM/atmDesign/ATMStates/IdleState.java
@@ -0,0 +1,13 @@
+package ATMStates;
+
+import ATMDesign.ATM;
+import ATMDesign.Card;
+
+public class IdleState extends ATMState {
+
+    @Override
+    public void insertCard(ATM atm, Card card) {
+        System.out.println("Card is inserted");
+        atm.setCurrentATMState(new HasCardState());
+    }
+}
---
+++ ATM/atmDesign/AmountWithdrawal/CashWithdrawProcessor.java
@@ -0,0 +1,22 @@
+package AmountWithdrawal;
+
+
+import ATMDesign.ATM;
+
+public abstract class CashWithdrawProcessor {
+
+    CashWithdrawProcessor nextCashWithdrawalProcessor;
+
+    CashWithdrawProcessor(CashWithdrawProcessor cashWithdrawalProcessor) {
+
+        this.nextCashWithdrawalProcessor = cashWithdrawalProcessor;
+
+    }
+
+    public void withdraw(ATM atm, int remainingAmount) {
+
+        if (nextCashWithdrawalProcessor != null) {
+            nextCashWithdrawalProcessor.withdraw(atm, remainingAmount);
+        }
+    }
+}
